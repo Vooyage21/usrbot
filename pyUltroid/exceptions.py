1367a4839af32d7a4fd919169b116eed391ab350
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-07 18:29:28 +0700

    update pyUltroid/exceptions.py

--- pyUltroid/exceptions.py
+++ pyUltroid/exceptions.py
@@ -13,10 +13,8 @@ Exceptions which can be raised by py-Ultroid Itself.
 class pyUltroidError(Exception):
     ...
 
-
 class DependencyMissingError(ImportError):
     ...
-
 
 class RunningAsFunctionLibError(pyUltroidError):
     ...
