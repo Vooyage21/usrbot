aa1a4135cf6c0ce37f9cfd3f27bff767f62856fe
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-07 18:13:39 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -17,8 +17,7 @@ _main_help_menu = [
         Button.inline(get_string("help_5"), data="uh_Addons_"),
         Button.inline(get_string("help_6"), data="uh_VCBot_"),
         Button.inline(get_string("help_7"), data="inlone"),
-            get_string("help_9"), url=f"https://t.me/{asst.me.username}?start=set"
-        ),
+        Button.inline(get_string("help_9"), url=f"https://t.me/{asst.me.username}?start=set"),
     ],
     [
       Button.inline(get_string("help_10"), data="close")
