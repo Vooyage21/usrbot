d888eb25852c4eea8d786609a0383b443320e62e
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-07 18:32:07 +0700

    update pyUltroid/fns/helper.py

--- pyUltroid/fns/helper.py
+++ pyUltroid/fns/helper.py
@@ -172,12 +172,12 @@ if run_as_module:
             output += "\n© @TeamUltroid"
             await eod(ok, f"✓ `Ultroid - Installed`: `{plug}` ✓\n\n{output}")
         elif plug in CMD_HELP:
-            output = f"Plugin Name-{plug}\n\n✘ Commands Available-\n\n"
+            output = f"Plugin Name-{plug}\n\nCommands Available-\n\n"
             output += str(CMD_HELP[plug])
             await eod(ok, f"✓ `Ultroid - Installed`: `{plug}` ✓\n\n{output}")
         else:
             try:
-                x = f"Plugin Name-{plug}\n\n✘ Commands Available-\n\n"
+                x = f"Plugin Name-{plug}\n\nCommands Available-\n\n"
                 for d in LIST[plug]:
                     x += HNDLR + d + "\n"
                 await eod(ok, f"✓ `Ultroid - Installed`: `{plug}` ✓\n\n`{x}`")
