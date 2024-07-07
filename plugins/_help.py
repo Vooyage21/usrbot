fbb7de86e259c0bb881f0a7e707b243516fa9d72
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-07 17:45:23 +0700

    update plugins/_help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -1,9 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 from telethon.errors.rpcerrorlist import (
     BotInlineDisabledError,
@@ -42,19 +36,19 @@ async def _help(ult):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @Usern4meDoesNotExist404"
                 await ult.eor(output)
             elif HELP.get("Addons") and plug in HELP["Addons"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Addons"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @Usern4meDoesNotExist404"
                 await ult.eor(output)
             elif HELP.get("VCBot") and plug in HELP["VCBot"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["VCBot"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @Usern4meDoesNotExist404"
                 await ult.eor(output)
             else:
                 try:
@@ -62,7 +56,7 @@ async def _help(ult):
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @TeamUltroid"
+                    x += "\n© @Usern4meDoesNotExist404"
                     await ult.eor(x)
                 except BaseException:
                     file = None
@@ -97,7 +91,7 @@ async def _help(ult):
                     elif HELP.get("VCBot") and file in HELP["VCBot"]:
                         for i in HELP["VCBot"][file]:
                             output += i
-                    output += "\n© @TeamUltroid"
+                    output += "\n© @Usern4meDoesNotExist404"
                     await ult.eor(output)
         except BaseException as er:
             LOGS.exception(er)
